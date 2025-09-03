45d9b723cdb14251847aadf4ca3d8fd92bcf6ecc
Azul Zarate <23302149@utfv.edu.mx>
2025-09-03 08:32:29 -0600

    se modifica error de module

--- src/app/pages/cirugias/cirugias.module.ts
+++ src/app/pages/cirugias/cirugias.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CirugiasRoutingModule } from './cirugias-routing.module';
-import { CirugiasComponent } from './cirugias.component';
 import { CirugiasListComponent } from './cirugias-list/cirugias-list.component';
 import { CirugiasFormComponent } from './cirugias-form/cirugias-form.component';
 
 
 @NgModule({
   declarations: [
-    CirugiasComponent,
     CirugiasListComponent,
     CirugiasFormComponent
   ],
