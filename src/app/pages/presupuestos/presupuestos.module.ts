c6386deb4bcb80c8778e918f3f45caeb2e6b2967
Mariana Almazan <24102003@utfv.edu.mx>
2025-09-03 08:44:14 -0600

    Se modifico error de module

--- src/app/pages/presupuestos/presupuestos.module.ts
+++ src/app/pages/presupuestos/presupuestos.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PresupuestosRoutingModule } from './presupuestos-routing.module';
-import { PresupuestosComponent } from './presupuestos.component';
 import { PresupuestosListComponent } from './presupuestos-list/presupuestos-list.component';
 import { PresupuestosFormComponent } from './presupuestos-form/presupuestos-form.component';
 
 
 @NgModule({
   declarations: [
-    PresupuestosComponent,
     PresupuestosListComponent,
     PresupuestosFormComponent
   ],
