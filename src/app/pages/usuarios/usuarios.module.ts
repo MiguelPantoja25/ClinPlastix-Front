fc77f3c117c375d65bb81a48b5f61b6154ed0d4b
DeividTaka <24102018@utfv.edu.mx>
2025-09-10 17:53:52 -0600

    Se agrega Formulario de Usuario

--- src/app/pages/usuarios/usuarios.module.ts
+++ src/app/pages/usuarios/usuarios.module.ts
@@ -3,19 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { UsuariosListComponent } from './usuarios-list/usuarios-list.component';
 import { UsuariosFormComponent } from './usuarios-form/usuarios-form.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UsuariosRoutingModule } from '../usuarios/usuarios-routing.module';
+
+// Angular Material
 import { MatTableModule } from '@angular/material/table';
-import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatOptionModule, MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDivider } from "@angular/material/divider";
+import { MatDividerModule } from "@angular/material/divider";
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UsuariosRoutingModule } from '../usuarios/usuarios-routing.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 @NgModule({
   declarations: [
     UsuariosListComponent,
@@ -23,23 +28,24 @@ import { UsuariosRoutingModule } from '../usuarios/usuarios-routing.module';
   ],
   imports: [
     CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatTableModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatCardModule,
-        MatIconModule,
-        MatButtonModule,
-        UsuariosRoutingModule,
-        MatSelectModule,
-        MatOptionModule,
-        MatDatepickerModule,
-        MatInputModule,
-        MatNativeDateModule,
-        MatDivider,
-        ReactiveFormsModule,
-        MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    UsuariosRoutingModule,
+
+    // Angular Material
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatSnackBarModule
   ]
 })
-export class UsuariosModule { }
+export class UsuariosModule {}
