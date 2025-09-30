4829d6611122d03470a873df80235d3fa0beedae
Mariana Almazan <24102003@utfv.edu.mx>
2025-09-30 14:57:14 -0600

    Se creo la pantalla principal de presupuestos

--- src/app/pages/presupuestos/presupuestos-routing.module.ts
+++ src/app/pages/presupuestos/presupuestos-routing.module.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { PresupuestosListComponent } from './presupuestos-list/presupuestos-list.component';
 import { PresupuestosFormComponent } from './presupuestos-form/presupuestos-form.component';
 
-const routes: Routes = [{ path: '', component: PresupuestosListComponent, data:{ title: 'Presupuestos'} }, 
-  { path: 'nuevo', component:PresupuestosFormComponent, data:{ title: 'Nuevo Presupuesto'}},
+const routes: Routes = [
+  { path: '', component: PresupuestosListComponent, data:{ title: 'Presupuestos'} }, 
+  { path: 'nuevo', component: PresupuestosFormComponent, data:{ title: 'Nuevo Presupuesto'} },
+  { path: 'editar/:id', component: PresupuestosFormComponent, data:{ title: 'Editar Presupuesto'} } 
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
