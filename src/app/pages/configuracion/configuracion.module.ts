1d694703d3ae11b875505d170d8ea7d587622104
Azul Zarate <23302149@utfv.edu.mx>
2025-10-07 09:14:28 -0600

    Se realizo formulario de configuracion con datos precargados.

--- src/app/pages/configuracion/configuracion.module.ts
+++ src/app/pages/configuracion/configuracion.module.ts
@@ -3,6 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ConfiguracionRoutingModule } from './configuracion-routing.module';
 import { ConfiguracionComponent } from './configuracion.component';
+//import { MatCard, MatCardTitle } from "@angular/material/card";
+
+import { MatCardModule } from "@angular/material/card"; //mat-card
+import { MatFormFieldModule } from '@angular/material/form-field'; //mat-form-field
+import { MatInputModule } from '@angular/material/input'; //mat input
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'; // botones
+
+import { ReactiveFormsModule } from '@angular/forms'; //uso del formulario
 
 
 @NgModule({
@@ -11,7 +20,13 @@ import { ConfiguracionComponent } from './configuracion.component';
   ],
   imports: [
     CommonModule,
-    ConfiguracionRoutingModule
+    ConfiguracionRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ]
 })
 export class ConfiguracionModule { }
